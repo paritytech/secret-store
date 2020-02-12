a40402eab4cdcdc4e37b54ffed186f683922c47c
Anton Gavrilov <AntonE.Gavrilov@gmail.com>
2020-02-12 12:14:02 +0100

    Name of the product changed in the license headers

--- src/key_server_cluster/io/shared_tcp_stream.rs
+++ src/key_server_cluster/io/shared_tcp_stream.rs
@@ -1,18 +1,18 @@
 // Copyright 2015-2020 Parity Technologies (UK) Ltd.
-// This file is part of Parity Ethereum.
+// This file is part of Parity Ethereum Secret Store.
 
-// Parity Ethereum is free software: you can redistribute it and/or modify
+// Parity Ethereum Secret Store is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Parity Ethereum is distributed in the hope that it will be useful,
+// Parity Ethereum Secret Store is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
+// along with Parity Ethereum Secret Store.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::sync::Arc;
 use std::net::Shutdown;

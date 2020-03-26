512ed1af55cc08604b7a42e1ec3f3793dc495795
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-03-26 12:48:30 +0300

    add more tasks to submit_transaction

--- bin/substrate/src/subcommands/submit_transaction.rs
+++ bin/substrate/src/subcommands/submit_transaction.rs
@@ -14,46 +14,47 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Secret Store.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 use clap::ArgMatches;
 use log::{error, info};
+use parity_crypto::publickey::{public_to_address, Public};
 use primitives::ServerKeyId;
 use crate::substrate_client::Client;
 
 /// All possible SecretStore transactions we support.
 pub enum SecretStoreTransaction {
-/*	// === Meta calls ===
+	// === Meta calls ===
 
 	/// Change owner.
-	ChangeOwner(crate::runtime::AccountId),
+//	ChangeOwner(crate::runtime::AccountId),
 	/// Claim id.
-	ClaimId(Address),
+//	ClaimId(Address),
 	/// Claim key.
 	ClaimKey(ServerKeyId),
 	/// Transfer key ownership.
-	TransferKey(ServerKeyId, Address),
+//	TransferKey(ServerKeyId, Address),
 
 	// === Key Server Set calls ===
 
 	/// Complete key server set initialization.
-	CompleteInitialization,
+//	CompleteInitialization,
 	/// Add key server to the set.
-	AddKeyServer(KeyServerId, String, u16),
+//	AddKeyServer(KeyServerId, String, u16),
 	/// Update key server from the set.
-	UpdateKeyServer(KeyServerId, String, u16),
+//	UpdateKeyServer(KeyServerId, String, u16),
 	/// Remove key server from the set.
-	UpdateKeyServer(KeyServerId),
+//	UpdateKeyServer(KeyServerId),
 
 	// === Key Server calls ===
-*/
+
 	/// Generate server key.
 	GenerateServerKey(ServerKeyId, u8),
 	/// Retrieve server key.
 	RetrieveServerKey(ServerKeyId),
-/*	/// Store document key.
+	/// Store document key.
 	StoreDocumentKey(ServerKeyId, Public, Public),
 	/// Retrieve document key shadow.
-	RetrieveDocumentKeyShadow(ServerKeyId, Public),*/
+	RetrieveDocumentKeyShadow(ServerKeyId, Public),
 }
 
 /// Submit Substrate transaction.
@@ -104,20 +105,54 @@ pub fn run(matches: &ArgMatches) {
 
 /// Parse transaction.
 fn parse_transaction(stransaction: &str) -> Result<SecretStoreTransaction, String> {
+	// to claim key, we only need to provide key id
+	let claim_key_regex = regex::Regex::new(r"ClaimKey\((.*)\)").expect("TODO");
+	if let Some(captures) = claim_key_regex.captures(stransaction) {
+		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str().trim_start_matches("0x"))
+			.map_err(|err| format!("{}", err))?;
+		return Ok(SecretStoreTransaction::ClaimKey(key_id));
+	}
+
+	// to generate server key, caller must provide: key id and threshold
 	let generate_server_key_regex = regex::Regex::new(r"GenerateServerKey\((.*),[ /t]*(.*)\)").expect("TODO");
 	if let Some(captures) = generate_server_key_regex.captures(stransaction) {
-		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str())
+		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str().trim_start_matches("0x"))
 			.map_err(|err| format!("{}", err))?;
-		let threshold = u8::from_str(captures.get(2).expect("TODO").as_str())
+		let threshold = u8::from_str(captures.get(2).expect("TODO").as_str().trim_start_matches("0x"))
 			.map_err(|err| format!("{}", err))?;
 		return Ok(SecretStoreTransaction::GenerateServerKey(key_id, threshold));
 	}
 
+	// to retrieve server key, caller must provide: key id
 	let retrieve_server_key_regex = regex::Regex::new(r"RetrieveServerKey\((.*)\)").expect("TODO");
 	if let Some(captures) = retrieve_server_key_regex.captures(stransaction) {
-		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str())
+		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str().trim_start_matches("0x"))
 			.map_err(|err| format!("{}", err))?;
 		return Ok(SecretStoreTransaction::RetrieveServerKey(key_id));
+	}
+
+	// to store document key, caller must provide: key id, common point and encrypted point
+	let store_document_key_regex = regex::Regex::new(r"StoreDocumentKey\((.*),[ /t]*(.*),[ /t]*(.*)\)")
+		.expect("TODO");
+	if let Some(captures) = store_document_key_regex.captures(stransaction) {
+		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str().trim_start_matches("0x"))
+			.map_err(|err| format!("{}", err))?;
+		let common_point = Public::from_str(captures.get(2).expect("TODO").as_str().trim_start_matches("0x"))
+			.map_err(|err| format!("{}", err))?;
+		let encrypted_point = Public::from_str(captures.get(3).expect("TODO").as_str().trim_start_matches("0x"))
+			.map_err(|err| format!("{}", err))?;
+		return Ok(SecretStoreTransaction::StoreDocumentKey(key_id, common_point, encrypted_point));
+	}
+
+	// to retrieve document key shadow, caller must provide: key id and its (requester) public key
+	let retrieve_document_key_shadow_regex = regex::Regex::new(r"RetrieveDocumentKeyShadow\((.*),[ /t]*(.*)\)")
+		.expect("TODO");
+	if let Some(captures) = retrieve_document_key_shadow_regex.captures(stransaction) {
+		let key_id = ServerKeyId::from_str(captures.get(1).expect("TODO").as_str().trim_start_matches("0x"))
+			.map_err(|err| format!("{}", err))?;
+		let requester_public = Public::from_str(captures.get(2).expect("TODO").as_str().trim_start_matches("0x"))
+			.map_err(|err| format!("{}", err))?;
+		return Ok(SecretStoreTransaction::RetrieveDocumentKeyShadow(key_id, requester_public));
 	}
 
 	Err("unknown call".into())
@@ -131,7 +166,19 @@ async fn process_transaction(
 	is_wait_processed: bool,
 	transaction: SecretStoreTransaction,
 ) -> Result<(), String> {
+	// TODO: even if ClaimKey fails, we still consider it successful
+	// TODO: we consider ClaimKey finalized even if it is not
+	
 	match transaction {
+		SecretStoreTransaction::ClaimKey(id) => process_generic_transaction(
+			&client,
+			is_wait_mined,
+			false,
+			false,
+			crate::runtime::SecretStoreCall::claim_key(id),
+			|_| true,
+			|_| true,
+		).await,
 		SecretStoreTransaction::GenerateServerKey(id, threshold) => process_generic_transaction(
 			&client,
 			is_wait_mined,
@@ -202,6 +249,140 @@ async fn process_transaction(
 				_ => false,
 			},
 		).await,
+		SecretStoreTransaction::StoreDocumentKey(id, common_point, encrypted_point) => process_generic_transaction(
+			&client,
+			is_wait_mined,
+			is_wait_finalized,
+			is_wait_processed,
+			crate::runtime::SecretStoreCall::store_document_key(id, common_point, encrypted_point),
+			move |event| match *event {
+				crate::runtime::Event::secretstore_runtime_module(
+					runtime_module::Event::DocumentKeyStoreRequested(req_id, _, req_cp, req_ep),
+				) if req_id == id && req_cp == common_point && req_ep == encrypted_point => true,
+				_ => false,
+			},
+			move |event| match *event {
+				crate::runtime::Event::secretstore_runtime_module(
+					runtime_module::Event::DocumentKeyStored(req_id),
+				) if req_id == id => {
+					info!(
+						target: "secretstore",
+						"Document key has been stored",
+					);
+					true
+				},
+				crate::runtime::Event::secretstore_runtime_module(
+					runtime_module::Event::DocumentKeyStoreError(req_id),
+				) if req_id == id => {
+					info!(
+						target: "secretstore",
+						"Document key store has failed",
+					);
+					true
+				},
+				_ => false,
+			},
+		).await,
+		SecretStoreTransaction::RetrieveDocumentKeyShadow(id, requester_public) => {
+			let mut common_portion_retrieved = false;
+			let mut threshold = 0xFFu8;
+			let mut personal_portions = HashMap::new();
+			let requester_address = public_to_address(&requester_public);
+
+			process_generic_transaction(
+				&client,
+				is_wait_mined,
+				is_wait_finalized,
+				is_wait_processed,
+				crate::runtime::SecretStoreCall::retrieve_document_key_shadow(id, requester_public),
+				move |event| match *event {
+					crate::runtime::Event::secretstore_runtime_module(
+						runtime_module::Event::DocumentKeyShadowRetrievalRequested(req_id, req_requester),
+					) if req_id == id && requester_address == req_requester => true,
+					_ => false,
+				},
+				move |event| match *event {
+					crate::runtime::Event::secretstore_runtime_module(
+						runtime_module::Event::DocumentKeyCommonRetrieved(
+							req_id,
+							req_requester,
+							req_common_point,
+							req_threshold,
+						),
+					) if req_id == id && requester_address == req_requester => {
+						common_portion_retrieved = true;
+						threshold = req_threshold;
+
+						info!(
+							target: "secretstore",
+							"Common portion of document key has been retrieved: threshold = {}, common_point = {:?}",
+							req_threshold,
+							req_common_point,
+						);
+						false
+					},
+					crate::runtime::Event::secretstore_runtime_module(
+						runtime_module::Event::DocumentKeyPersonalRetrieved(
+							req_id,
+							req_requester,
+							req_decrypted_point,
+							ref req_shadow,
+						),
+					) if
+						common_portion_retrieved &&
+						req_id == id &&
+						requester_address == req_requester
+					=> {
+						let shadows = personal_portions
+							.entry(req_decrypted_point)
+							.or_insert_with(|| {
+								info!(
+									target: "secretstore",
+									"Adding new personal entry candidate: {:?}",
+									req_decrypted_point,
+								);
+
+								Vec::new()
+							});
+
+						shadows.push(req_shadow.clone());
+						if shadows.len() == threshold as usize + 1 {
+							info!(
+								target: "secretstore",
+								"Received last required shadow for personal entry: {:?}",
+								req_decrypted_point,
+							);
+							info!(
+								target: "secretstore",
+								"Finals shadows list: {:?}",
+								shadows,
+							);
+
+							true
+						} else {
+							info!(
+								target: "secretstore",
+								"Received shadow for personal entry: {:?}. {} More required",
+								req_decrypted_point,
+								threshold as usize + 1 - shadows.len(),
+							);
+
+							false
+						}
+					},
+					crate::runtime::Event::secretstore_runtime_module(
+						runtime_module::Event::DocumentKeyShadowRetrievalError(req_id, req_requester),
+					) if req_id == id && requester_address == req_requester => {
+						info!(
+							target: "secretstore",
+							"Document key shadow retrieval has failed",
+						);
+						true
+					},
+					_ => false,
+				},
+			).await
+		},
 	}
 }
 

592f3caede616ebc1cdfd6e5e6f8f356dace1b32
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-03-30 12:50:21 +0300

    print full hash

--- substrate-service/src/lib.rs
+++ substrate-service/src/lib.rs
@@ -82,7 +82,7 @@ pub enum SecretStoreCall {
 /// Substrate blockchain.
 pub trait Blockchain: 'static + Send + Sync {
 	/// Block hash type.
-	type BlockHash: Clone + std::fmt::Display + Send + Sync;
+	type BlockHash: Clone + std::fmt::Debug + Send + Sync;
 	/// Blockchain event type.
 	type Event: MaybeSecretStoreEvent + Send;
 	/// Block events stream type.
@@ -228,7 +228,7 @@ pub fn start_service<B, E, TP, KSrv, KStr>(
 
 impl<B: Blockchain> std::fmt::Display for SubstrateBlock<B> {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-		write!(f, "{}", self.block_hash)
+		write!(f, "{:?}", self.block_hash)
 	}
 }
 
